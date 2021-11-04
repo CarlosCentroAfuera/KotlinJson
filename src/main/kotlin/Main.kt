e3125e6178d28449bc115acaf87d5d1e15e80d44
Carlos <carlostena@centroafuera.onmicrosoft.com>
2021-11-04 13:23:31 +0100

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    val p1 = Personaje("Paquito", 1)
+
+    println(p1)
+    println(p1.toJson())
+
+    val json1 = "{\"nombre\":\"Rodolfo\",\"edad\":2}"
+    val p2 = Personaje.fromJson(json1)
+
+    println(p2)
+}
